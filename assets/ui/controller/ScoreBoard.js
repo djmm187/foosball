a813f4ed35845b2f583b5b68da403d5e7cc5c18f
djmm187 <djmm187@gmail.com>
2013-12-04 11:22:24 -0800

    quick updates

--- assets/ui/controller/ScoreBoard.js
+++ assets/ui/controller/ScoreBoard.js
@@ -1,30 +1,69 @@
 /**
 * @class ui.controller.ScoreBoard
-*	simple controller for ScoreBoard functionality
+*   simple controller for ScoreBoard functionality
 */
 
 Ext.define('ui.controller.ScoreBoard', {
-	extend: 'Ext.app.Controller',
-	init: function() {
-		var me = this;
+    extend: 'Ext.app.Controller',
+    init: function() {
+        var me = this;
 
-		this.control({
-			"#mainView toolbar [itemId=addGame]": {
-				click: this.onAddGameClick
-			},
-			"#mainView [itemId=scoreBoard]": {
-				afterrender: this.afterGameStoreLoad
-			}
-		});
-	},
+        this.control({
+            "#mainView toolbar [itemId=addGame]": {
+                click: this.onAddGameClick
+            },
+            "#mainView [itemId=scoreBoard]": {
+                afterrender: this.afterGameStoreLoad
+            }
+        });
+    },
 
-	//click event handler bound to 'Add Game' button
-	onAddGameClick: function (btn) {
+    //click event handler bound to 'Add Game' button
+    onAddGameClick: function (btn) {
 
-		var grid = btn.up('panel'),
-		form;
+        var me = this,
+            grid = btn.up('panel'),
+            form;
 
-        form = Ext.create('ui.view.form.Game');
+        form = Ext.create('ui.view.form.Game', {
+                buttons: [
+                    '->',
+                    {
+                        text: 'Save New Game',
+                        itemId: 'saveForm',
+                        handler: function (btn) {
+
+                            var panel = this.up('form'),
+                            form = panel.getForm(),
+                            values = form.getValues(),
+                            grid = Ext.ComponentQuery.query("#scoreBoard")[0],
+                            players = Ext.getCmp('playerWins').getStore();
+
+                            if (!form.isValid()) return;
+
+                            panel.setLoading('Saving new Game...');
+
+                            //add our new record
+                            grid.getStore().add(values);
+
+                            //update our score board
+                            me.updateScoreBoardRanks(players, values);
+
+                            //remove loader and close the window
+                            panel.setLoading(false);
+                            panel.up('window').close();
+                            return;
+                        }
+                    },
+                    {
+                        text: 'Cancel',
+                        itemId: 'cancel',
+                        handler: function (btn) {
+                            btn.up('window').close();
+                        }
+                    }
+                ]
+            });
 
         Ext.create('ui.view.util.Modal', {
             title: "Add a New Game",
@@ -34,46 +73,54 @@ Ext.define('ui.controller.ScoreBoard', {
 
     //fetch game data
     afterGameStoreLoad: function(cmp) {
-    	var me = this,
-    	rankGrid = Ext.getCmp('playerWins');
+        var me = this,
+        rankGrid = Ext.getCmp('playerWins');
 
-    	//loader and populate our scoreboard
-    	rankGrid.setLoading(true);
-    	setTimeout(function(){
-    		me.populateRankings(rankGrid);
-    	}, 200);
+        //loader and populate our scoreboard
+        rankGrid.setLoading(true);
+        setTimeout(function(){
+            me.populateRankings(rankGrid);
+        }, 200);
     },
 
     //populate our ranking board
     populateRankings: function(cmp) {
 
-    	var gameScores = Ext.StoreMgr.lookup('games'),
-    	players = Ext.StoreMgr.lookup('players');
-    	
-    	//tally up the init game results
-    	gameScores.each(function(item, index, count) {
+        var me = this,
+            gameScores = Ext.StoreMgr.lookup('games'),
+            players = Ext.StoreMgr.lookup('players'),
+            rec;
+        
+        //tally up the init game results
+        gameScores.each(function(item, index, count) {
+            rec = item.raw;
 
-    		var rec = item.raw,
-    		winner, targetRec; 
+            //no need to compare equal scores. no one can brag about a stalemate
+            if (rec.score1 !== rec.score2) {
+                me.updateScoreBoardRanks(players, rec);
+            }
+        });
 
-    		//no need to compare equal scores. no one can brag about a stalemate
-    		if (rec.score1 !== rec.score2) {
-
-    			//lets find the winners
-    			winner = (rec.score1 > rec.score2) ? rec.player1 : rec.player2;
-
-    			//find our winning player from our store
-    			targetRec = players.findRecord('name', winner);
-
-    			//update winning count
-    			targetRec.set('wins', parseInt(targetRec.get('wins')) + 1);
-    		}
-    	});
-
-    	cmp.setLoading(false);
+        cmp.setLoading(false);
     },
 
-    onRemoveClick: function(grid, rowIndex){
+    //update our scoreboard
+    updateScoreBoardRanks: function (store, rec) {
+
+        var winner, 
+            targetRec;
+
+        //lets find the winners
+        winner = (rec.score1 > rec.score2) ? rec.player1 : rec.player2;
+
+        //find our winning player from our store
+        targetRec = store.findRecord('name', winner);
+
+        //update winning count
+        targetRec.set('wins', parseInt(targetRec.get('wins')) + 1);
+    },
+
+    onRemoveClick: function (grid, rowIndex){
         this.getStore().removeAt(rowIndex);
     }
 });
--- assets/ui/view/form/Game.js
+++ assets/ui/view/form/Game.js
@@ -48,51 +48,6 @@ Ext.define('ui.view.form.Game', {
             allowBlank: false
         }];
 
-        this.buttons = [
-		'->',
-		{
-			text: 'Save',
-			itemId: 'saveForm',
-			handler: function (btn) {
-
-
-				var panel = this.up('form'),
-				form = panel.getForm(),
-				values = form.getValues(),
-				grid = Ext.ComponentQuery.query("#scoreBoard")[0],
-				players = Ext.getCmp('playerWins').getStore(),
-				winner, prec;
-
-				if (form.isValid()) {
-
-					panel.setLoading('Saving new Game...');
-
-					//add our new record
-					grid.getStore().add(values);
-
-					//update our score board
-					winner = (values.score1 > values.score2) ? values.player1 : values.player2;
-					prec = players.findRecord('name', winner);
-					prec.set('wins', parseInt(prec.get('wins')) + 1);
-
-				} else {
-					return;
-				}
-
-				panel.setLoading(false);
-				panel.up('window').close();
-				return;
-			}
-		},
-		{
-			text: 'Cancel',
-			itemId: 'cancel',
-			handler: function (btn) {
-				btn.up('window').close();
-			}
-		}
-	],
-
 		this.callParent(arguments);
 	}
 
